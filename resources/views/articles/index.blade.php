599866fd6a4cbb6ca8a7698f8be7a7abc3d3e67c
tomoki23 <tmdesign.0223@gmail.com>
2022-11-30 21:29:02 +0900

    一覧ページ更新

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,8 +5,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>一覧ページ</title>
+  <style>
+    th,td {
+      padding: 6px 24px;
+      text-align: center
+    }
+    table {
+      margin-bottom: 12px;
+    }
+  </style>
 </head>
 <body>
-  <p>一覧ページです</p>
+  <h1>一覧ページ</h1>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>タイトル</th>
+        <th>本文</th>
+        <th>投稿日</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($articles as $article)
+      <tr>
+        <td>{{ $article->title }}</td>
+        <td>{{ $article->body }}</td>
+        <td>{{ $article->created_at }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  <a href="{{ route('articles.create') }}">投稿ページ</a>
 </body>
 </html>
