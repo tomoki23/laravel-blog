6384d0268a2f9f66446697924e58a7b5e13bb6a7
tomoki23 <tmdesign.0223@gmail.com>
2022-11-30 22:47:52 +0900

    削除のルート設定

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,4 @@ Route::post('/articles', [ArticleController::class, 'store'])->name('articles.st
 Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
 Route::get('/articles/{id}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
 Route::post('/articles/{id}', [ArticleController::class, 'update'])->name('articles.update');
+Route::delete('/articles/{id}', [ArticleController::class, 'destroy'])->name('articles.destroy');
