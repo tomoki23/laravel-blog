fdec09adbb8d93716259f8d7ad21c459bb448839
tomoki23 <tmdesign.0223@gmail.com>
2022-11-30 21:29:18 +0900

    storeのルート設定

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
