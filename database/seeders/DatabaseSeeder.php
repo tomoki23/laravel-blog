0a388b4c54dd5362192fd20836898725e18b8daf
tomoki23 <tmdesign.0223@gmail.com>
2022-11-30 21:28:27 +0900

    seederコメント

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ArticleSeeder::class,
+            // ArticleSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
