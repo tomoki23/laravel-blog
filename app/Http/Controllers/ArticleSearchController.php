7296f6583f4ea760152a197241867139c3368ba8
tomoki23 <tmdesign.0223@gmail.com>
2022-12-10 15:22:14 +0900

    searhアクション更新

--- app/Http/Controllers/ArticleSearchController.php
+++ app/Http/Controllers/ArticleSearchController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleSearchController extends Controller
 {
     public function search(Request $request)
     {
-        return view('search.index');
+        $searchWord = $request->input('searhWord', '');
+
+        $searchArticles = Article::where('id', '=', '3')
+            // ->where('body', 'LIKE', 'テスト')
+            ->get()
+            ->all();
+
+        return view('search.index', compact('searchArticles'));
     }
 }
