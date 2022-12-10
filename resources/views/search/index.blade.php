229401cf8dbc54a8e3231b69cbfd13c3800fa978
tomoki23 <tmdesign.0223@gmail.com>
2022-12-10 15:14:50 +0900

    検索結果表示

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -7,6 +7,32 @@
   <title>検索結果</title>
 </head>
 <body>
-  <p>検索結果</p>
+  <h1>検索結果</h1>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>タイトル</th>
+        <th>本文</th>
+        <th>投稿日</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($searchArticles as $article)
+      <tr>
+        <td>{{ $article->title }}</td>
+        <td>{{ $article->body }}</td>
+        <td>{{ $article->created_at }}</td>
+        <td style="border:none;">
+          <a href="{{ route('articles.show',[$article->id]) }}">詳細</a>
+          <form action="{{ route('articles.destroy',[$article->id]) }}" method="post">
+            @method('DELETE')
+            @csrf
+            <button type="submit">削除</button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 </body>
 </html>
